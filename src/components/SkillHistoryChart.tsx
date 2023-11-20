ed653ee4b8d075dfe279af6924ad38131eb7d4fb
Collin Brennan <collinbrennan8@gmail.com>
2023-11-20 16:48:07 -0500

    Chart can now display all history or last 6 months

--- src/components/SkillHistoryChart.tsx
+++ src/components/SkillHistoryChart.tsx
@@ -2,6 +2,9 @@ import { ChartData } from 'chart.js'
 import { Line } from 'react-chartjs-2'
 import { LogDocData } from '../utils/types'
 import { timeFromSeconds } from '../utils/dateAndTime'
+import 'chartjs-adapter-dayjs-4'
+import { Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
 function getChartData(logs: LogDocData[] | undefined): ChartData<'line'> {
   const chartData = {
@@ -9,52 +12,47 @@ function getChartData(logs: LogDocData[] | undefined): ChartData<'line'> {
     datasets: [
       {
         label: 'hours',
-        data: [] as number[],
+        data: [] as { x: number; y: number }[],
         borderColor: 'rgba(126, 211, 33, 1)',
         backgroundColor: 'rgba(126, 211, 33, 1)',
       },
     ],
   }
 
-  function addDataPoint(date: Date, hours: number) {
-    chartData.labels.unshift(
-      date.toLocaleDateString(undefined, {
-        month: 'short',
-        year: 'numeric',
-      })
-    )
-    chartData.datasets[0].data.unshift(hours)
+  function pushMonthlyHours(timestamp: Timestamp, hours: number) {
+    chartData.datasets[0].data.push({
+      x: timestamp.toMillis(),
+      y: hours,
+    })
   }
 
   if (logs) {
-    var totalHoursInDay = 0
-    var date = logs[0].createdAt.toDate()
+    var totalHours = 0
+    var timestamp = logs[0].createdAt
+    var date = timestamp.toDate()
     var month = date.getMonth()
     var year = date.getFullYear()
-
     logs.forEach((log, logIndex) => {
-      const { hours, minutes } = timeFromSeconds(log.seconds)
       const logDate = log.createdAt.toDate()
       const logMonth = logDate.getMonth()
       const logYear = logDate.getFullYear()
-
-      // Add hours to current month
+      const { hours, minutes } = timeFromSeconds(log.seconds)
       if (logMonth === month && logYear === year) {
-        totalHoursInDay += hours + minutes / 60
+        totalHours += hours + minutes / 60
       } else {
-        // Push previous month, reset to current day
-        addDataPoint(date, totalHoursInDay)
-        totalHoursInDay = hours + minutes / 60
+        pushMonthlyHours(timestamp, totalHours)
+        totalHours = hours + minutes / 60
+        timestamp = log.createdAt
         date = logDate
         month = logMonth
         year = logYear
       }
-      // Push if last log in array
       if (logIndex === logs.length - 1) {
-        addDataPoint(date, totalHoursInDay)
+        pushMonthlyHours(timestamp, totalHours)
       }
     })
   }
+
   return chartData
 }
 
@@ -63,30 +61,53 @@ type SkillFrequencyChartProps = {
 }
 
 const TimeHistoryChart = ({ logs }: SkillFrequencyChartProps) => {
+  const [isViewAll, setIsViewAll] = useState(false)
+
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-[99%] h-72">
-        <Line
-          data={getChartData(logs)}
-          options={{
-            plugins: { legend: { display: false } },
-            elements: {
-              point: {
-                radius: 8,
-                hoverRadius: 10,
-              },
-            },
-            scales: {
-              y: {
-                ticks: {
-                  callback: (val) => (val === 1 ? val + ' hr' : val + ' hrs'),
+    <div className="w-full flex flex-col">
+      <button
+        onClick={() => setIsViewAll((prevIsViewAll) => !prevIsViewAll)}
+        className="flex self-end text-blue-500 underline"
+      >
+        {!isViewAll ? 'View all' : 'View last 6 months'}
+      </button>
+      <div className="w-full flex justify-center">
+        <div className="w-[99%] h-72">
+          <Line
+            data={getChartData(logs)}
+            options={{
+              plugins: { legend: { display: false } },
+              elements: {
+                point: {
+                  radius: 8,
+                  hoverRadius: 10,
                 },
               },
-            },
-            responsive: true,
-            maintainAspectRatio: false,
-          }}
-        />
+              scales: {
+                x: {
+                  type: 'time',
+
+                  time: {
+                    displayFormats: { month: !isViewAll ? 'MMMM' : "MMM 'YY" },
+                    tooltipFormat: 'MMMM',
+                    unit: 'month',
+                    round: 'month',
+                  },
+                  min: !isViewAll
+                    ? Timestamp.now().toMillis() - 13149000000
+                    : undefined,
+                },
+                y: {
+                  ticks: {
+                    callback: (val) => (val === 1 ? val + ' hr' : val + ' hrs'),
+                  },
+                },
+              },
+              responsive: true,
+              maintainAspectRatio: false,
+            }}
+          />
+        </div>
       </div>
     </div>
   )
